ce8682afc63fdd4e9e976fe3996500d9fa0dbdb9
Dawn Alderson <grassrootshairstudio@gmail.com>
2026-05-25 04:15:53 +0000

    Visual edits

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -46,7 +46,7 @@ export default function HeroSection() {
             <h1 className="font-logo md:text-6xl lg:text-7xl text-coral leading-[1.1] mb-2 text-base">for busy homes
 
             </h1>
-            <h1 className="font-heading font-light text-2xl md:text-3xl lg:text-4xl leading-[1.2] mb-8 text-[hsl(var(--primary))] lowercase">Because no one is actually all caught up.
+            <h1 className="font-heading font-light text-2xl md:text-3xl lg:text-4xl leading-[1.2] mb-8 text-[hsl(var(--primary))] normal-case">Because no one is actually all caught up.
 
             </h1>
           </motion.div>
