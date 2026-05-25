719e2240aff24c2e7064aed5b034fcb9cb1f8781
Dawn Alderson <grassrootshairstudio@gmail.com>
2026-05-25 04:03:24 +0000

    File changes

--- src/components/shared/WaveDivider.jsx
+++ src/components/shared/WaveDivider.jsx
@@ -7,7 +7,7 @@ export default function WaveDivider({ fill = '#F7F4F0', flip = false, className 
         viewBox="0 0 1440 80"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
-        className="w-full h-16 md:h-20"
+        style={{ display: 'block', width: '100%', height: '80px' }}
       >
         <path
           d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
