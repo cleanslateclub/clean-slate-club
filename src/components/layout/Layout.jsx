bbc9e760ecfd1ac457f0a0eda55271eef1c86660
Dawn Alderson <grassrootshairstudio@gmail.com>
2026-05-28 05:01:38 +0000

    File changes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="min-h-screen bg-cream">
       <Navbar />
