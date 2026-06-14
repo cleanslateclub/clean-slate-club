9afe22371ec514af72eb640142b36c16fcc1838f
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-14 00:22:37 -0400

    Refine wave divider component

--- src/components/shared/WaveDivider.jsx
+++ src/components/shared/WaveDivider.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function WaveDivider({ fill = '#F7F4F0', flip = false, className = '' }) {
   return (
-    <div className={`w-full overflow-hidden leading-none ${flip ? 'rotate-180' : ''} ${className}`}>
+    <div className={`w-full overflow-hidden leading-none ${className}`} aria-hidden="true">
       <svg
-        viewBox="0 0 1440 80"
+        viewBox="0 0 1440 72"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
-        style={{ display: 'block', width: '100%', height: '80px' }}
+        style={{ display: 'block', width: '100%', height: '72px', transform: flip ? 'scaleX(-1)' : 'none' }}
       >
         <path
-          d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
+          d="M0,38 C180,70 360,12 540,34 C720,56 900,70 1080,30 C1260,-8 1380,18 1440,34 L1440,72 L0,72 Z"
           fill={fill}
         />
       </svg>
