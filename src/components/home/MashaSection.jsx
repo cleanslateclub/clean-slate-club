98839b3173e90df7b9914516b93b229442c1f93d
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-14 00:22:53 -0400

    Use solid Masha section background

--- src/components/home/MashaSection.jsx
+++ src/components/home/MashaSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AnimatedSection from '../shared/AnimatedSection';
 export default function MashaSection() {
   return (
-    <section id="about" className="py-24 lg:py-36 relative overflow-hidden" style={{ background: 'linear-gradient(180deg, #fdf5f3 0%, #F1F1F1 100%)' }}>
+    <section id="about" className="py-24 lg:py-36 relative overflow-hidden" style={{ background: '#fdf5f3' }}>
       <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-12">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center">
           {/* Image */}
