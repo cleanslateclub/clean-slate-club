65bbf86902a683ce80c3ec9cf14ed1877009e308
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-14 00:48:15 -0400

    Tighten homepage section padding

--- src/components/home/MashaSection.jsx
+++ src/components/home/MashaSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AnimatedSection from '../shared/AnimatedSection';
 export default function MashaSection() {
   return (
-    <section id="about" className="py-24 lg:py-36 relative overflow-hidden" style={{ background: '#fdf5f3' }}>
+    <section id="about" className="py-16 lg:py-24 relative overflow-hidden" style={{ background: '#fdf5f3' }}>
       <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-12">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center">
           {/* Image */}
