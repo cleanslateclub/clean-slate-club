4289a71ec5a0329c018397201bc94b67e219b8b8
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-14 00:25:23 -0400

    Add homepage wave dividers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,26 @@ import ServicesPreview from '../components/home/ServicesPreview';
 import TrustSection from '../components/home/TrustSection';
 import ConsultSection from '../components/home/ConsultSection';
 import TestimonialsSection from '../components/home/TestimonialsSection';
-import MembershipPreview from '../components/home/MembershipPreview'; // FIX: removed .jsx extension to match all other imports
+import MembershipPreview from '../components/home/MembershipPreview';
 import ServiceAreaSection from '../components/home/ServiceAreaSection';
 import FAQSection from '../components/home/FAQSection';
+import WaveDivider from '../components/shared/WaveDivider';
 
 export default function Home() {
   return (
-    <main> {/* FIX: <main> instead of <div> for accessibility + SEO */}
+    <main>
       <HeroSection />
+      <WaveDivider fill="#fdf5f3" />
       <MashaSection />
+      <WaveDivider fill="#F1F1F1" flip />
       <ServicesPreview />
+      <WaveDivider fill="#FFFFFF" />
       <TrustSection />
+      <WaveDivider fill="#fdf5ec" flip />
       <ConsultSection />
+      <WaveDivider fill="#FFE5D9" />
       <TestimonialsSection />
+      <WaveDivider fill="#F1F1F1" flip />
       <MembershipPreview />
       <ServiceAreaSection />
       <FAQSection />
