790dbf2e0036a5e0498830eda9123985293baa50
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-22 20:31:10 -0400

    Add FAQ route

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Services from './pages/Services.jsx';
 import Memberships from './pages/Memberships.jsx';
+import FAQ from './pages/FAQ.jsx';
 import BookNow from './pages/BookNow';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminLogin from './pages/AdminLogin';
@@ -60,6 +61,7 @@ const AuthenticatedApp = () => {
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/memberships" element={<Memberships />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/book" element={<BookNow />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin-login" element={<AdminLogin />} />
