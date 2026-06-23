ae869c0cf255d66fb96785905363e21c212b5ce3
cleanslateclub <cleanslateclubpa@gmail.com>
2026-06-23 17:38:27 -0400

    Add global block style brand color strip

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,6 +4,29 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import WorkInProgressBanner from '@/components/shared/WorkInProgressBanner';
 
+const brandBarColors = [
+  '#DFE3A2',
+  '#CAE7B9',
+  '#F3DE8A',
+  '#EFB988',
+  '#EB9486',
+  '#B58A90',
+  '#7E7F9A',
+  '#97A7B3',
+];
+
+function BrandColorBar() {
+  return (
+    <div className="fixed inset-x-0 top-0 z-[80] h-3 overflow-hidden sm:h-4" aria-hidden="true">
+      <div className="flex h-full w-full">
+        {brandBarColors.map(color => (
+          <span key={color} className="h-full flex-1" style={{ background: color }} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default function Layout() {
   const { pathname, search } = useLocation();
 
@@ -13,6 +36,7 @@ export default function Layout() {
 
   return (
     <div className="min-h-screen bg-cream">
+      <BrandColorBar />
       <WorkInProgressBanner />
       <Navbar />
       <main>
